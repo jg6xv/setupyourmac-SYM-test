74ad527c89fe4e6144beab4be5b785424ad13824
Jason Galambos <jg6xv@virginia.edu>
2024-03-14 11:49:23 -0400

    Debugging UNKMDL

--- SetupYourMac.bash
+++ SetupYourMac.bash
@@ -2587,22 +2587,46 @@ elif [[ "${welcomeDialog}" == "userInput" ]]; then
                     if [ "$(/usr/sbin/sysctl -in hw.optional.arm64)" = 1 ] && /usr/sbin/sysctl -n machdep.cpu.brand_string | /usr/bin/grep -q 'Apple' && /usr/bin/uname -v | /usr/bin/grep -q 'ARM64'
                     then
                         marketModel="$(/usr/libexec/PlistBuddy -c 'print 0:product-name' /dev/stdin <<< "$(/usr/sbin/ioreg -ar -k product-name)")"
+                        if [ "$debugMode" = "verbose" ]; then
+                            updateScriptLog "EXA - ARM detected!"
+                            updateScriptLog "EXA - marketModel is reporting \"$marketModel\""
+                        fi
                     else
+                        if [ "$debugMode" = "verbose" ]; then
+                            updateScriptLog "EXA - Intel detected!"
+                        fi
                         if ! [ -e "$plistsp" ]
                         then
+                            if [ "$debugMode" = "verbose" ]; then
+                                updateScriptLog "EXA - Marketing plist not detected! Opening About This Mac..."
+                            fi
                             # This is a REALLY stupid way of doing it, but the model name doesn't get filled in unless
                             #   'About This Mac' gets opened.
-                            /usr/bin/open '/System/Library/CoreServices/Applications/About This Mac.app'; /bin/sleep 1
+                            /usr/bin/open '/System/Library/CoreServices/Applications/About This Mac.app'; /bin/sleep 5
+                            if [ "$debugMode" = "verbose" ]; then
+                                updateScriptLog "EXA - Opening System Information..."
+                            fi
                             /usr/bin/pkill -ail 'System Information'; /bin/sleep 5
+                            if [ "$debugMode" = "verbose" ]; then
+                                updateScriptLog "EXA - Killing tasks..."
+                            fi
                             /usr/bin/killall cfprefsd; /bin/sleep 5
                         fi
                         marketModel="$(/usr/libexec/PlistBuddy -c "print 'CPU Names':$srlnmbr-en-US_US" "$plistsp" 2> /dev/null)"
+                        if [ "$debugMode" = "verbose" ]; then
+                            updateScriptLog "EXA - Fetching Marketing Model..."
+                            updateScriptLog "EXA - Marketing Model reporting as \"$marketModel\""
+                        fi
                     fi
 
                     # If the above didn't work, print to log
                     if [ -z "$marketModel" ]
                     then
                         updateScriptLog "EXA - Market Model not found! Model Identifier: <result>$(/usr/sbin/sysctl -n hw.model)</result>"
+                    elif
+                        if [ "$debugMode" = "verbose" ]; then
+                            updateScriptLog "EXA - Marketing model string is not null."
+                        fi
                     fi
 
                     updateScriptLog "EXA - Market Model is reporting as \"$marketModel\"."
